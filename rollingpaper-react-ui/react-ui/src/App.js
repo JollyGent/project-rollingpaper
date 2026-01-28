73faee8b00d59858ad69b19033463a81ca16c5bc
JollyGent <pchhana@hotmail.com>
2026-01-28 21:33:35 +0000

    Use React to start working on the Frontend

---
+++ rollingpaper-react-ui/react-ui/src/App.js
@@ -0,0 +1,48 @@
+
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+  const [messages, setMessage] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('api/v1/messages')
+      .then(response => response.json())
+      .then(data => {
+        setMessage(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+//<img src={logo} className="App-logo" alt="logo" />
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>RollingPaper</h1>
+
+        <input name="Message" placeholder="Say something about me..."/>
+        <button>Post</button>
+
+        <div className="App-intro">
+          {messages.map(message =>
+            <div key={message.id}>
+              {message.message}
+            </div>
+          )}
+        </div>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
