8aaf8f37b777ea7ee54f6ab12b44111510431205
JollyGent <pchhana@hotmail.com>
2026-01-29 04:05:12 +0000

    UI change for the website, using CSS

--- rollingpaper-react-ui/react-ui/src/App.js
+++ rollingpaper-react-ui/react-ui/src/App.js
@@ -9,41 +9,37 @@ const App = () => {
   const [messages, setMessage] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   setLoading(true);
+  useEffect(() => {
+    setLoading(true);
 
-  //   fetch('api/v1/messages')
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       setMessage(data);
-  //       setLoading(false);
-  //     })
-  // }, []);
+    fetch('api/v1/messages')
+      .then(response => response.json())
+      .then(data => {
+        setMessage(data);
+        setLoading(false);
+      })
+  }, []);
 
-  // if (loading) {
-  //   return <p>Loading...</p>;
-  // }
-//<img src={logo} className="App-logo" alt="logo" />
-  // const inputStyle = input{
-  //   border: none,
-  //   outline: none,
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+<img src={logo} className="App-logo" alt="logo" />
 
-  // }
   return (
     <div className="App">
       <header className="App-header">
-        <h1>RollingPaper</h1>
+        <h1 style={{color:'#1C1C1C'}}>RollingPaper</h1>
 
         <input className="Message" placeholder="Say something about me..."/>
         <button>Post</button>
 
-        {/* <div className="App-intro">
+        <div className="App-intro">
           {messages.map(message =>
             <div key={message.id}>
               {message.message}
             </div>
           )}
-        </div> */}
+        </div>
 
 
       </header>
