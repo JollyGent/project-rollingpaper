5453a87e5b71059caa8b116ae0d9e5bd9f940219
JollyGent <pchhana@hotmail.com>
2026-01-30 20:19:55 +0000

    Configurations in the backend to get ready to deploy

--- src/main/java/com/example/rollingpaper/RollingPaperApplication.java
+++ src/main/java/com/example/rollingpaper/RollingPaperApplication.java
@@ -3,6 +3,7 @@ package com.example.rollingpaper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// http://localhost:3000
 @SpringBootApplication
 public class RollingPaperApplication {
 
